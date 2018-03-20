0c5dce6605c3436ce360c877e05f8f4bc49f2591
dedsec404 <muhammad.wahidin@students.amikom.ac.id>
2018-03-20 09:17:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,27 @@
-    <?php
-    $file = 'https://www.googleapis.com/drive/v3/files/15NonV4RqbJYFJFgnVOYJAvCKX1s4o-YS?key=AIzaSyBDxIV0cTa6fpeHzkZ5B763dcA7FhjoTYs&alt=media';
-    $fp = @fopen($file, 'rb');
-    $size   = filesize($file); // File size
-    $length = $size;           // Content length
-    $start  = 0;               // Start byte
-    $end    = $size - 1;       // End byte
-    header('Content-type: video/mp4');
-    //header("Accept-Ranges: 0-$length");
-    header("Accept-Ranges: bytes");
-    if (isset($_SERVER['HTTP_RANGE'])) {
-        $c_start = $start;
-        $c_end   = $end;
-        list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-        if (strpos($range, ',') !== false) {
-            header('HTTP/1.1 416 Requested Range Not Satisfiable');
-            header("Content-Range: bytes $start-$end/$size");
-            exit;
-        }
-        if ($range == '-') {
-            $c_start = $size - substr($range, 1);
-        }else{
-            $range  = explode('-', $range);
-            $c_start = $range[0];
-            $c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
-        }
-        $c_end = ($c_end > $end) ? $end : $c_end;
-        if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
-            header('HTTP/1.1 416 Requested Range Not Satisfiable');
-            header("Content-Range: bytes $start-$end/$size");
-            exit;
-        }
-        $start  = $c_start;
-        $end    = $c_end;
-        $length = $end - $start + 1;
-        fseek($fp, $start);
-        header('HTTP/1.1 206 Partial Content');
-    }
-    header("Content-Range: bytes $start-$end/$size");
-    header("Content-Length: ".$length);
-    $buffer = 1024 * 8;
-    while(!feof($fp) && ($p = ftell($fp)) <= $end) {
-        if ($p + $buffer > $end) {
-            $buffer = $end - $p + 1;
-        }
-        set_time_limit(0);
-        echo fread($fp, $buffer);
-        flush();
-    }
-    fclose($fp);
-    exit();
-    ?>
+<?php
+$ID_File = $_SERVER['QUERY_STRING'];
+session_start();
+$VideoHash = md5(uniqid());
+$_SESSION[$VideoHash] = "https://www.googleapis.com/drive/v3/files/$ID_File?key=AIzaSyBDxIV0cTa6fpeHzkZ5B763dcA7FhjoTYs&alt=media";
+?> 
 
+<head>
+<link href="http://vjs.zencdn.net/6.6.3/video-js.css" rel="stylesheet">
+</head>
+
+<body>
+<video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
+poster="ChiisaiNime.png" data-setup="{}">
+<source src="stream.php?h=<?php echo $VideoHash;?>" type='video/mp4'>
+</video>
+
+<body>
+  <video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
+  poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+    <source src="stream.php?h=<?php echo $VideoHash;?>" type='video/mp4'>
+  </video>
+
+
+ <script src="http://vjs.zencdn.net/6.6.3/video.js"></script>
+
+</body>
